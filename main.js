63545eb3b067796300a060f95cde011f52af4a7c
masa0mi <masaomi.nagafuji@gmail.com>
2025-08-08 14:26:12 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+let mediaRecorder;
+let audioChunks = [];
+
+function startRecording() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+            audioChunks = [];
+            mediaRecorder.ondataavailable = event => {
+                audioChunks.push(event.data);
+            };
+            mediaRecorder.onstop = () => {
+                const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+                const audioUrl = URL.createObjectURL(audioBlob);
+                const audio = new Audio(audioUrl);
+                audio.play();
+            };
+            console.log("🎙 Recording started");
+        });
+}
+
+function stopRecording() {
+    if (mediaRecorder) {
+        mediaRecorder.stop();
+        console.log("🛑 Recording stopped");
+    }
+}
